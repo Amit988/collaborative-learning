8db019b440c8321bfbb6a808d1c61d7bcdc81a35
Neesham <neeshamraghav0@gmail.com>
2021-12-25 20:02:07 +0530

    error

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
-from django.conf.urls import url
+#from django.conf.urls import url
 from accounts.views import index, donation
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
